3b1a1b607345425177b7db10fec2fcb0361428e8
cpyli <cpyli@uw.edu>
2017-08-28 19:30:33 -0700

    Committing partially completed assignment.

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,5 +1,6 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.*;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by LaunchCode
@@ -24,6 +28,10 @@ public class JobController {
     public String index(Model model, int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
+
+        Job someJob = jobData.findById(id);
+
+        model.addAttribute("job", someJob);
 
         return "job-detail";
     }
@@ -41,7 +49,25 @@ public class JobController {
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
 
-        return "";
+        if (errors.hasErrors()) {
+            return "new-job";
+        }
+
+        Job newJob = null;
+
+        Employer aEmployer = jobData.getEmployers().findById(jobForm.getEmployerId());
+        Location aLocation = jobData.getLocations().findById(jobForm.getLocationId());
+        CoreCompetency aCoreCompetency = jobData.getCoreCompetencies().findById(jobForm.getCoreCompetencyId());
+        PositionType aPositionType = jobData.getPositionTypes().findById(jobForm.getPositionTypeId());
+
+        newJob.setEmployer(aEmployer);
+        newJob.setLocation(aLocation);
+        newJob.setCoreCompetency(aCoreCompetency);
+        newJob.setPositionType(aPositionType);
+
+        jobData.add(newJob);
+
+        return "redirect:/job?id=" + newJob.getId();
 
     }
 }
